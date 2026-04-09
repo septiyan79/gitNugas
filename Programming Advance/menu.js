da7c766c867ecb624e05d31a8badfe63c1d9abd4
Mizoku <septiyan79.se@gmail.com>
2026-04-09 21:53:32 +0700

    complete file rumus.js

---
+++ Programming Advance/menu.js
@@ -0,0 +1,8 @@
+// menu.js
+import { kalkulator } from './rumus.js';
+import readline from 'readline';
+// Membuat interface untuk input dari terminal
+const inputUser = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
